9fc44851be310d699c6359ed0193411ec46ff13c
Ignisooqa <72989883+Ignisooqa@users.noreply.github.com>
2020-10-16 19:26:23 +0300

    Update Neo.py

--- Neo.py
+++ Neo.py
@@ -1,1 +1,19 @@
-import telebotTOKEN = "1348439222:AAHp9D8pIAUC4_7nqnOYQfhjhsWEmnBikIQ"bot = telebot.TeleBot(TOKEN)@bot.message_handler(content_types=["text"])def start(message):    print(message)    print(message.chat.id)    chat_id = message.chat.id    bot.send_message(chat_id, "HELLO")bot.polling(none_stop=True)  # Запускает бота
+import telebot
+
+TOKEN = "1348439222:AAHp9D8pIAUC4_7nqnOYQfhjhsWEmnBikIQ"
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def start(message):
+    print(message)
+    print(message.chat.id)
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "HELLO")
+
+
+bot.polling(none_stop=True)  # Запускает бота
+
+print("dsf")
+
